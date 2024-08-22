61d0f576817179ad3495d32cf296be448cb7a869
George Pickering <9803299+bigpick@users.noreply.github.com>
2024-08-22 17:35:39 -0400

    fix: no send_commit_status

--- scripts/lint/python.sh
+++ scripts/lint/python.sh
@@ -7,30 +7,18 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ###
 # Check that a given src/test directory passes code linting checks.
-#
-# Side effects:
-#    * Sends commit status check at CODE_LINT_CHECK with results.
 ###
 lint(){ local src_dir="${1-src}" test_dir="${2-test}"
     local rc=0
 
     # Requires standard project layout; all must have symlinked common ruff
     # config to root repo's ruff.toml
-        ruff check --config "${src_dir}/../ruff.toml" "${src_dir}" "${test_dir}" || rc=$?
-        ruff format --config "${src_dir}/../ruff.toml" --check "${src_dir}" "${test_dir}" || rc=$?
-
-    if [[ "${CI-}" != "true" ]]; then
-        exit $rc
-    fi
-
-    if [[ "${rc}" == "0" ]]; then
-        send_commit_status "success" "${CODE_LINT_CHECK_DESC}" "${CODE_LINT_CHECK}"
-    else
-        send_commit_status "failure" "FAIL: Ensure 'ruff check src test' returns successful." "${CODE_LINT_CHECK_DESC}" "${CODE_LINT_CHECK}"
-    fi
+    ruff check --config "${src_dir}/../ruff.toml" "${src_dir}" "${test_dir}" || rc=$?
+    ruff format --config "${src_dir}/../ruff.toml" --check "${src_dir}" "${test_dir}" || rc=$?
 
     if [[ "${rc}" != "0" ]]; then
         print_err "Linting failed."
-        exit 1
     fi
+
+    exit $rc
 }
